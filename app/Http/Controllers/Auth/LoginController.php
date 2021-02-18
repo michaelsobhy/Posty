7d0be930a0132f74fa19b999c3b7afcf94605f46
michaelsobhy <michael.sobhy01@gmail.com>
2021-02-18 14:13:35 +0200

    First Commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['guest']); //if not guest (if singed in) redirect to home (as he can not register)
+    }
+
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        //validation
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required' //automatically checks for 'password' and 'password_confirmation'
+        ]);
+
+        if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
+            return back()->with('status', 'Invalid login details');
+        } else {
+            return redirect()->route('dashboard');
+        }
+    }
+}
